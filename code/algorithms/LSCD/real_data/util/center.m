61da2cbbe39c1206aeae3093539b44a632f2c365
parkyunjin <123270293+parkyunjin@users.noreply.github.com>
2025-10-09 22:24:29 -0400

    Delete code directory

--- code/algorithms/LSCD/real_data/util/center.m
+++
@@ -1,9 +0,0 @@
-function [value] = center(X)
-% center both the columns and rows of a matrix
-     n = size(X, 1);
-     X = X - ones(n, 1) * (ones(1, n) * X / n); 
-     X = X - X / n * ones(n, 1) * ones(1, n);
-     value = X;
-end
-
-     
--- code/algorithms/LSCD/real_data/util/sigmoid.m
+++
@@ -1,4 +0,0 @@
-function [value] = sigmoid(x)
-% compute the sigmoid function
-	value = 1 ./ (1 + exp(-x));
-end
